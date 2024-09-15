07a6ed7516b49ac17d7cf86c4cf1c04c2bc58653
birdkron <js8215001@gmail.com>
2024-09-15 15:22:50 -0600

    Agregada validacion para aplicar migracion

--- UsuarioAPI/Program.cs
+++ UsuarioAPI/Program.cs
@@ -40,6 +40,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<UsuarioContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.UseHttpsRedirection();
 app.UseOutputCache();
 app.UseAuthorization();
