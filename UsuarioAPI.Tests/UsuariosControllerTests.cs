aefbda3281ed554816a9102f1f8459e1410ac185
robertoher76 <robertoher76@gmail.com>
2024-09-15 16:17:07 -0600

    Prueba Unitario Usuarios

--- UsuarioAPI.Tests/UsuariosControllerTests.cs
+++ UsuarioAPI.Tests/UsuariosControllerTests.cs
@@ -3,6 +3,7 @@ using UsuarioAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
+using System.Configuration;
 
 namespace UsuarioAPI.Tests
 {
@@ -11,18 +12,19 @@ namespace UsuarioAPI.Tests
         [Fact]
         public void GetUsuario_RetornaUsuario_CuandoIdEsValido()
         {
-            //Arrange
+            // Arrange: Configuración del contexto y las dependencias externas
             var context = Setup.GetDatabaseContext();
 
             var configuration = ConfigurationOptions.Parse("redis-13961.c251.east-us-mz.azure.redns.redis-cloud.com:13961,password=C30UCMzzkVaB2UVvKpRpCQeclYDEmYLn", true);
-            var redis = ConnectionMultiplexer.Connect(configuration);
+            var redis = ConnectionMultiplexer.Connect(configuration);            
 
+            // Inicializar el controlador con el contexto y Redis
             var controller = new UsuariosController(context, redis);
 
-            //Act
+            // Act: Llamar al método del controlador
             var result = controller.GetUsuario(1);
 
-            //Assert
+            // Assert: Verificar que se obtiene el resultado correcto
             var actionResult = Assert.IsType<ActionResult<Usuario>>(result);
             var returnValue = Assert.IsType<Usuario>(actionResult.Value);
             Assert.Equal("SysAdmin", returnValue.Nombre);
